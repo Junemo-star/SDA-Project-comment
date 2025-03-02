24515d614807e4befc5393e3218c54226288cc58
saran <saranyapong272547@gmail.com>
2025-03-03 00:29:27 +0700

    config port v.3 server

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['5ec3a2de9cb0254b7e8f28493c143230b146f48f6d55997ecb3e47a0e54148b0', '0a25c79d9c1d8969e1b8e11690b993879d06240fe9ba0805e4117f957c6a4434']),
   },
 });
