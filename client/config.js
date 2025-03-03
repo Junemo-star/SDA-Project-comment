fdd0d8c4154337f72c90ffd521d6c015c744c7fe
saran <saranyapong272547@gmail.com>
2025-03-03 21:59:05 +0700

    config file config.js

--- client/config.js
+++ client/config.js
@@ -1,10 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
-  // isProd,
-  // serverUrlPrefix: isProd ? 'https://wd10.cloud-workshop.online' : 'http://localhost:1337'
   isProd,
-  serverUrlPrefix: isProd ? 'https://sda-556323513019.asia-southeast1.run.app' : 'https://sda-556323513019.asia-southeast1.run.app'
+  serverUrlPrefix: isProd ? 'http://34.87.40.14' : 'http://localhost:1337'
 }
 
 export default config;
