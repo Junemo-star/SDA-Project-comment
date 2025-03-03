63f8f35b7e67e0b6f9a79d644cddfb0f63d98239
saran <saranyapong272547@gmail.com>
2025-03-03 17:41:40 +0700

    config v5 server

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,7 +1,5 @@
-import path from 'path';
-
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
+  const client = env('DATABASE_CLIENT', 'mysql');
 
   const connections = {
     mysql: {
@@ -25,32 +23,34 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', '/cloudsql/sda-project-449715:asia-southeast1:strapi-db'),
+        //host: env('DATABASE_HOST', '/cloudsql/sda-project-449715:asia-southeast1:strapi-db'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_db'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD'), // ลบ default password
-        ssl: env.bool('DATABASE_SSL', false) ? {
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-          ca: env('DATABASE_SSL_CA', undefined),
-        } : false,
-        schema: env('DATABASE_SCHEMA', 'public'),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'), // ลบ default password
+        ssl: env.bool('DATABASE_SSL', false),
+        // ssl: env.bool('DATABASE_SSL', false) ? {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        //   ca: env('DATABASE_SSL_CA', undefined),
+        // } : false,
+        // schema: env('DATABASE_SCHEMA', 'public'),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      //pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
+    // sqlite: {
+    //   connection: {
+    //     filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    //   },
+    //   useNullAsDefault: true,
+    // },
   };
 
   return {
     connection: {
       client,
       ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      //acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
 };
